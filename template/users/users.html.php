34fdcc46ea6ed6cbb3fb8ab7bfc94bf1e99e5841
bigdap2p <46713230+bigdap2p@users.noreply.github.com>
2019-03-14 09:56:08 +0100

    Update users.html.php

--- template/users/users.html.php
+++ template/users/users.html.php
@@ -2,11 +2,28 @@
 
 <title><?= $title ?></title>
 
-<?php foreach ($users as $value): ?>
 
-    <?=  $value->getEmail() ?>
+
+    <div class="row col-lg-2 col-md-2">
+        <form method="post" action="" >
+<?php
+
+foreach ($users as $value): ?>
+
+    <?php
+  echo  "<input type=\"checkbox\" name=".$value->getEmail()." value=".$value->getEmail()." id=".$value->getEmail().">
+    <label for=".$value->getEmail()."> ".$value->getEmail()." </label><br>";
+
+    ?>
 
 <?php endforeach ?>
+
+        <input type="text" name="nom_discussion" class="form-control" placeholder="nom_discussion">
+        <input type="text" name="admin_discussion" class="form-control" placeholder="admin_discussion email">
+        <input type="submit" name="createDiscussion" value="Create Discussion" class="btn btn-primary">
+
+     </form>
+</div>
 
 
 <?php include __DIR__ . "/../end.html.php" ?>
